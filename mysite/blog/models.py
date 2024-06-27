d4cbf072b699e556692bc94ac4d5dfc9e1b0faab
Django-B <django.ppp@gmail.com>
2024-06-27 12:00:26 +0300

    Добавлено теггирование постов

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager # type: ignore
 
 
 class PublishedManager(models.Manager):
@@ -27,6 +28,8 @@ class Post(models.Model):
 
     objects = models.Manager() # менеджер, применяемый по умолчанию
     published = PublishedManager() # конкретно-прикладной менеджер
+
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
 
+from taggit.models import Tag
+
 
 def post_share(request, post_id):
     # Извлечь пост по его идентификатору id
@@ -35,6 +37,32 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
+
 class PostListView(ListView):
     """
     Альтернативное представление списка постов
